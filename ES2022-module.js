b7a4067ece77049a0e2363ff0d667acea0f445e1
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-04-28 22:18:40 +0100

    Updates to include ES2022 variants.
    Also bumps the version of node in CI and the actions used therein.

---
+++ ES2022-module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  extends: require('path').join(__dirname, 'ES2022.js'),
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  }
+};
---
+++ ES2022.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  extends: require('path').join(__dirname, 'ES2021.js'),
+  env: {
+    es2022: true
+  }
+};
